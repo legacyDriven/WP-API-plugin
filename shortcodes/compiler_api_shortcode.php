32df3f42068eecce3598f4166c743b9e31a76e20
Eugeniusz <58223087+legacyDriven@users.noreply.github.com>
2024-02-01 01:48:29 +0100

    Initial commit

--- shortcodes/compiler_api_shortcode.php
+++ shortcodes/compiler_api_shortcode.php
@@ -1,1 +1,10 @@
 <?php
+function my_code_compiler_shortcode() {
+    ob_start();
+    ?>
+    <!-- Tutaj wklejasz HTML -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/prism/9000.0.1/prism.js"></script>
+    <?php
+    return ob_get_clean();
+}
+add_shortcode('code_compiler', 'my_code_compiler_shortcode');
